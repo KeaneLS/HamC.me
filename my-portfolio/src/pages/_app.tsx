eeeccd357f9d0c375dd71bfe4df5c87e828fad97
Keane <kleeshan@uwaterloo.ca>
2025-02-11 05:30:10 -0500

    debugging react

--- my-portfolio/src/pages/_app.tsx
+++ my-portfolio/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
-import '../styles/global.css';
+import '@/styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
