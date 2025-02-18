e4d6e7f35e7246d191b7d711719d382a597fcfa7
Keane <kleeshan@uwaterloo.ca>
2025-02-17 23:08:33 -0500

    Analytics functionality

--- my-portfolio/src/pages/_app.tsx
+++ my-portfolio/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
 import '@/styles/global.css';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
